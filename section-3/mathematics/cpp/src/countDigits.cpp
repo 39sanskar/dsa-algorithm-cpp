e37cd4d233a00c98433233373a27f72765ce7118
39sanskar <mishrasanskar496@gmail.com>
2026-02-22 23:25:25 +0530

    update

--- section-3/mathematics/cpp/src/countDigits.cpp
+++ section-3/mathematics/cpp/src/countDigits.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int countDigits(int x);
+int countDigits(int x); // function declaration 
 int main() {
   int x;
   cout << "Enter the Number: ";
@@ -9,12 +9,14 @@ int main() {
 
   if (x < 0) {
     cout << "Invalid number!" << endl;
-    return 0;
+    return 1;
   }
   
   cout << "DigitCount: " << countDigits(x) << endl;
 
 }
+
+// function definition 
 int countDigits(int x) {
   int result = 0;
 
@@ -25,4 +27,18 @@ int countDigits(int x) {
   return result;
 }
 
+// If function is below main() → declaration needed
+// If function is above main() → no declaration needed
+
 // Time Complexity: theta(d); d => no. of d digit then its going to run d times.
+
+/*
+
+return → exits function + gives back a value
+
+return 0 in main() → success signal
+Program finished successfully
+You are sending status 0 to the operating system
+return 1 → often used for error or true
+
+*/
